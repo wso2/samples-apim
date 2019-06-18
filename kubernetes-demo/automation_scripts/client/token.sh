873215dfb49e0f4346bc2bcdbe9c72adf68c4967
Andrea Perera <andriperera.98@gmail.com>
2019-06-18 12:10:48 +0530

    Add License headers

--- kubernetes-demo/automation_scripts/client/token.sh
+++ kubernetes-demo/automation_scripts/client/token.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# ------------------------------------------------------------------------
+# Copyright 2019 WSO2, Inc. (http://wso2.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+# ------------------------------------------------------------------------
+
+#this script automates Generating access token
+
 ECHO=`which echo`
 function echoBold () {
     ${ECHO} -e $'\e[1m'"${1}"$'\e[0m'
