390b86310e20b018e7b472b6479444950cfef64d
Renuka Fernando <renukapiyumal@gmail.com>
2022-05-14 07:02:56 +0530

    Add thumbnail for microservices APIs Remove k8s ingress resources

--- apim-tutorial/choreo-connect/utils/get-cert-pem-to-secret.sh
+++
@@ -1,5 +0,0 @@
-openssl x509 -inform der -in 20.197.56.160.cer -out 20.197.56.160.pem
-openssl x509 -inform der -in \*.apim.com.cer -out star.apim.com.pem
-kubectl create secret generic enforcer-truststore-test --from-file 20.197.56.160.pem --from-file star.apim.com.pem
-kubectl get secret enforcer-truststore-test -oyaml
-kubectl delete secret enforcer-truststore-test
