22f6dd4e34b98f5cbf211aeaa2f11f1bbcc195b5
Arunan Sugunakumar <arunans23@gmail.com>
2022-05-15 12:57:09 +0530

    Update publish-usage.sh

--- apim-tutorial/resources/publish-usage.sh
+++ apim-tutorial/resources/publish-usage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-access_token=$(curl -X POST https://dev.apim.com/oauth2/token -H 'Authorization: Basic RWk3WXU4TXp4cmxCOVdXVzFyWldsVENqQ1FjYTpOTzFUTWhNN080SVhnalg0TWg4bGdEcjFwWFlh' -d 'grant_type=password&username=admin&password=admin&scope=apim:monetization_usage_publish' -k | jq -r '.access_token')
+access_token=$(curl -X POST https://dev.apim.com/oauth2/token -H 'Authorization: Basic bzBPZ0ZhN1JLVm43WVp0WFdRMHJRMUhHeDY0YTpmeUZmRW9mR2ZDejVocmNtYW9lQW14VFhoR1lh' -d 'grant_type=password&username=admin&password=admin&scope=apim:monetization_usage_publish' -k | jq -r '.access_token')
 
 echo $access_token
 
