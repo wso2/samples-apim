76d04d5ab8368afff4c9aea5c057a9da0e3ceb45
dushani <dushaniw@wso2.com>
2022-06-22 07:32:03 +0530

    update starwars subscriptions to graphql-transport-ws

--- graphql-backend/server.js
+++ graphql-backend/server.js
@@ -1,30 +1,60 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { createServer } from 'http';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 import cors from 'cors';
 
 import schema from './data/schema';
 
 const PORT = 8080;
 
+// create express and HTTP server
 const app = express();
-
 app.use('*', cors());
 app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({limit: '100mb'}));
-
-const server = new ApolloServer({ 
-  schema, 
-  subscriptions: { path: "/graphql" }
-});
-
-server.applyMiddleware({ app });
-
 const httpServer = createServer(app);
 
-server.installSubscriptionHandlers(httpServer);
+// create websocket server
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/graphql',
+});
+
+
+// Save the returned server's info so we can shut down this server later
+const serverCleanup = useServer({ schema, connectionInitWaitTimeout : 10000}, wsServer);
+
+
+// create apollo server
+const apolloServer = new ApolloServer({
+  schema,
+  plugins: [
+    // Proper shutdown for the HTTP server.
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+
+    // Proper shutdown for the WebSocket server.
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
+    },
+  ],
+});
+
+(async () =>  {
+  await apolloServer.start();
+})();
+
+apolloServer.applyMiddleware({ app });
 
 httpServer.listen(PORT, () => {
-  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
-  console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`);
+  console.log(`🚀 Server ready at http://localhost:${PORT}${apolloServer.graphqlPath}`);
+  console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${apolloServer.subscriptionsPath}`);
 });
