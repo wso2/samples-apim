dc87e030853c622efc43942ce1d10e1b2611b5fd
wasuradananjith <wasuradananjith@gmail.com>
2022-06-28 09:26:22 +0530

    Add books and reservation services

---
+++ apim-certifications/backends/books-service/src/test/java/wso2/booksservice/BooksserviceApplicationTests.java
@@ -0,0 +1,11 @@
+package wso2.booksservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class BooksserviceApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
